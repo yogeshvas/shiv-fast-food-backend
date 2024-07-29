ee85f25126489bdc3cfb081ec363a02d706c5071
Yogesh Vashisth <writetokhair@gmail.com>
2024-07-30 00:19:53 +0530

    done

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express from 'express';
-import { getDashboardData } from './controllers/admin.js';
-import connectDB from './db/connectMongo.js';
-import kitchenRoutes from './routes/kitchen.js';
-import menuRoutes from './routes/menu.js';
-import orderRoutes from './routes/order.js';
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { getDashboardData } from "./controllers/admin.js";
+import connectDB from "./db/connectMongo.js";
+import kitchenRoutes from "./routes/kitchen.js";
+import menuRoutes from "./routes/menu.js";
+import orderRoutes from "./routes/order.js";
 
 dotenv.config();
 
@@ -13,9 +13,9 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: 'http://localhost:5173', // Allow your frontend origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  origin: "http://localhost:5173", // Allow your frontend origin
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
   credentials: true, // Allow credentials
 };
 
@@ -24,10 +24,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.get('/', getDashboardData);
-app.use('/api/v1/menu', menuRoutes);
-app.use('/api/v1/order', orderRoutes);
-app.use('/api/v1/kitchen', kitchenRoutes);
+app.get("/", getDashboardData);
+app.use("/api/v1/menu", menuRoutes);
+app.use("/api/v1/order", orderRoutes);
+app.use("/api/v1/kitchen", kitchenRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
